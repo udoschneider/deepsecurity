1fb0fd01c0cc2cdfac4e577f7bc851a18d77b93d
Udo Schneider <Udo.Schneider@homeaddress.de>
2013-04-19 21:05:09 +0200

    Commit strip_comments unit test

--- test/test_savon_helper.rb
+++ test/test_savon_helper.rb
@@ -1,10 +1,15 @@
 require 'test/unit'
 require 'deepsecurity'
 
-class TestSudoku < Test::Unit::TestCase
+class TestSavonHelper < Test::Unit::TestCase
   def test_blank
     assert_equal false, "abc".blank?
     assert_equal true, "".blank?
     assert_equal true, nil.blank?
+  end
+
+  def test_strip_comments
+    assert_equal "test", "test".strip_comments
+    assert_equal "test ", "test #123".strip_comments
   end
 end
