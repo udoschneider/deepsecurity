c252c2458d9750b4c711ed748fbd0589a7ad6355
Udo Schneider <Udo.Schneider@homeaddress.de>
2013-05-13 16:30:43 +0200

    Identifiers with underscores

--- lib/deepsecurity/transport_objects/time_filter.rb
+++ lib/deepsecurity/transport_objects/time_filter.rb
@@ -6,11 +6,14 @@ module DeepSecurity
   class TimeFilter < TransportObject
 
     attr_datetime_accessor :rangeFrom,
-                           "Time range start to filter computers by."
+                           "Time range start to filter computers by." ,
+                           :range_from
     attr_datetime_accessor :rangeTo,
-                           "Time range end to filter computers by."
+                           "Time range end to filter computers by.",
+                           :range_to
     attr_datetime_accessor :specificTime,
-                           "Specific time to filter computers by."
+                           "Specific time to filter computers by.",
+                           :specific_time
     # attr_integer_accessor :host_group_id
     # attr_integer_accessor :host_id
     # attr_integer_accessor :security_profile_id
@@ -50,18 +53,26 @@ module DeepSecurity
     def self.custom_range(datetime_range)
       instance = self.new()
       instance.type = :custom_range
-      instance.rangeFrom = datetime_range.first
-      instance.rangeTo = datetime_range.last
+      instance.range_from = datetime_range.first
+      instance.range_to = datetime_range.last
       instance
+    end
+
+    # Return a new instance for the given datetime.
+    # @deprecated Please use {#specific_time} instead
+    # @param datetime [DateTime]
+    # @return [TimeFilter]
+    def self.specificTime(datetime)
+      self.specific_time(datetime)
     end
 
     # Return a new instance for the given datetime.
     # @param datetime [DateTime]
     # @return [TimeFilter]
-    def self.specificTime(datetime)
+    def self.specific_time(datetime)
       instance = self.new()
-      instance.type = :specificTime
-      instance.specificTime = datetime
+      instance.type = :specific_time
+      instance.specific_time = datetime
       instance
     end
 
